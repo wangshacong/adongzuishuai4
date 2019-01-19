2f55e79fe7138b9c8c24139281cdad399109461f
xxd <1060675611@qq.com>
2019-01-19 11:54:46 +0800

    的

--- resources/views/gong/head.blade.php
+++ resources/views/gong/head.blade.php
@@ -1,9 +1,10 @@
 <div class="head">
-	<div class="logo"><a href="/"><img src="" alt="" /></a></div>
+		<div class="logo"><a href="/"><img src="" alt="" /></a></div>
 	<div class="share"> </div>
 </div>
 <div class="nav">
 	<ul>
+			
 		<li><a href="/" class="home">首页</a></li>
 		@foreach($fenlei as $v)
 		<li><a href='/fenlei/{{$v['id']}}'>{{$v['fenlei_name']}}</a></li>
